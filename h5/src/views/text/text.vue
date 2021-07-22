75d8388d299e76b5ba002098ccf405f89a9a5481
liuchunxiao <liuchunxiao@djbx.com>
2021-07-22 16:57:21 +0800

    更改后台管理系统模板

---
+++ h5/src/views/text/text.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="wscn-http404-container">
+    ceshi123123
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Page404',
+  computed: {
+    message() {
+      return 'The webmaster said that you can not enter this page...'
+    }
+  },
+  created(){
+    console.log("dayin")
+    console.log(this.$router)
+  }
+}
+</script>

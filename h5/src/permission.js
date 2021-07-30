3e91acb2fd41cde3c0c56453bd0a349f5d3c1120
UR901006 <UR901006ZHANGJIAQI@DJBX.COM>
2021-07-30 13:53:33 +0800

    修改路由守卫，创建登录页面，规范化代码

--- h5/src/permission.js
+++ h5/src/permission.js
@@ -1,63 +1,45 @@
 import router from './router'
-import store from './store'
+// import store from './store'
 import { Message } from 'element-ui'
 import NProgress from 'nprogress' // progress bar
 import 'nprogress/nprogress.css' // progress bar style
 import getPageTitle from '@/utils/get-page-title'
+import { getToken } from '@/utils/auth'
 
 NProgress.configure({ showSpinner: false }) // NProgress Configuration
 
 const whiteList = ['/login'] // no redirect whitelist
 
 router.beforeEach(async(to, from, next) => {
-    // start progress bar
-    NProgress.start()
+  // start progress bar
+  NProgress.start()
 
-    // set page title
-    document.title = getPageTitle(to.meta.title)
+  // set page title
+  document.title = getPageTitle(to.meta.title)
 
-    // determine whether the user has logged in
-    NProgress.done()
-    next()
-
-    /* if (hasToken) {
-        if (to.path === '/login') {
-            // if is logged in, redirect to the home page
-            next({ path: '/' })
-            NProgress.done()
-        } else {
-            const hasGetUserInfo = store.getters.name
-            if (hasGetUserInfo) {
-                next()
-            } else {
-                try {
-                    // get user info
-                    await store.dispatch('user/getInfo')
-
-                    next()
-                } catch (error) {
-                    // remove token and go to login page to re-login
-                    await store.dispatch('user/resetToken')
-                    Message.error(error || 'Has Error')
-                    next(`/login?redirect=${to.path}`)
-                    NProgress.done()
-                }
-            }
-        }
+  // determine whether the user has logged in
+  const hasToken = getToken() !== undefined
+  if (hasToken) {
+    if (to.path === '/login') {
+      // if is logged in, redirect to the home page
+      Message.info('Already logged in, no need to log in again')
+      next({ path: '/' })
+      // next({ path: from.path }) // 或许这个更好用？但是可能会有个警告
     } else {
-
-        if (whiteList.indexOf(to.path) !== -1) {
-            // in the free login whitelist, go directly
-            next()
-        } else {
-            // other pages that do not have permission to access are redirected to the login page.
-            next(`/login?redirect=${to.path}`)
-            NProgress.done()
-        }
-    } */
+      next()
+    }
+  } else {
+    if (whiteList.indexOf(to.path) !== -1) {
+      // in the free login whitelist, go directly
+      next()
+    } else {
+      // other pages that do not have permission to access are redirected to the login page.
+      next(`/login?redirect=${to.path}`)
+    }
+  }
 })
 
 router.afterEach(() => {
-    // finish progress bar
-    NProgress.done()
+  // finish progress bar
+  NProgress.done()
 })

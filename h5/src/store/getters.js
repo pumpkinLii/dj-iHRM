16e5b1b4ff9fc35dc0bcd2eb702419b7bea78dbe
UR901006 <UR901006ZHANGJIAQI@DJBX.COM>
2021-07-30 10:41:52 +0800

    规范化代码
    Signed-off-by: UR901006 <UR901006ZHANGJIAQI@DJBX.COM>

--- h5/src/store/getters.js
+++ h5/src/store/getters.js
@@ -1,5 +1,5 @@
 const getters = {
-    sidebar: state => state.app.sidebar,
-    device: state => state.app.device,
+  sidebar: state => state.app.sidebar,
+  device: state => state.app.device
 }
 export default getters

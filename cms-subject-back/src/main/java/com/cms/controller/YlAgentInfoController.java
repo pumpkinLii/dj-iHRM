9b441aaa610062b28935cdb7bc63d9ed305d895a
UR901008 <200911mm@>
2021-08-03 14:16:39 +0800

    测试接口

--- cms-subject-back/src/main/java/com/cms/controller/YlAgentInfoController.java
+++ cms-subject-back/src/main/java/com/cms/controller/YlAgentInfoController.java
@@ -1,7 +1,9 @@
 package com.cms.controller;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @CrossOrigin
-@RequestMapping("/test")
+@RequestMapping("test")
 @Api("养老渠道-人员管理-人员入司与信息维护")
 public class YlAgentInfoController {
+
+    @PostMapping("/s")
+    @ApiOperation("测试接口")
+    public String test(){
+        return "true";
+    }
+
 }

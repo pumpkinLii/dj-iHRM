3389e604d7bd2fd157373bc2ba005b2295256d23
zhanhaoze <zhanhaoze@djbx.com>
2021-08-03 13:44:52 +0800

    实习课题基础框架

---
+++ cms-subject-back/src/main/java/com/cms/controller/YlAgentInfoController.java
@@ -0,0 +1,18 @@
+package com.cms.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author :zhanhaoze
+ * @description :
+ * @create :2021-08-03
+ */
+@RestController
+@CrossOrigin
+@RequestMapping("/test")
+@Api("养老渠道-人员管理-人员入司与信息维护")
+public class YlAgentInfoController {
+}

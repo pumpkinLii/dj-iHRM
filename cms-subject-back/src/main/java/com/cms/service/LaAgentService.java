1fdb32d0214eb6945e50525f599a1141a68d0ba7
zhangxiaocheng <ur901007zhangxiaocheng@djbx.com>
2021-08-04 10:00:42 +0800

    人员录入模块

---
+++ cms-subject-back/src/main/java/com/cms/service/LaAgentService.java
@@ -0,0 +1,7 @@
+package com.cms.service;
+
+import com.cms.pojo.LaAgentPojo;
+
+public interface LaAgentService {
+    public boolean laAgentSubmit(LaAgentPojo laAgent);
+}
---
+++ cms-subject-back/src/main/java/com/cms/service/impl/LaAgentServiceImpl.java
@@ -0,0 +1,49 @@
+package com.cms.service.impl;
+
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.cms.dao.LaAgentDao;
+import com.cms.entity.YlLaAgentEntity;
+import com.cms.pojo.LaAgentPojo;
+import com.cms.service.LaAgentService;
+import org.springframework.stereotype.Service;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Service
+public class LaAgentServiceImpl extends ServiceImpl<LaAgentDao, YlLaAgentEntity> implements LaAgentService {
+    @Override
+    public boolean laAgentSubmit(LaAgentPojo laAgent){
+        YlLaAgentEntity ylLaAgentEntity = new YlLaAgentEntity();
+        ylLaAgentEntity.setAgentCode(laAgent.getAgentCode());
+        ylLaAgentEntity.setEmployDate(laAgent.getEmployDate());
+        ylLaAgentEntity.setAgentGrade(laAgent.getAgentGrade());
+        ylLaAgentEntity.setInitGrade(laAgent.getAgentGrade());
+        ylLaAgentEntity.setAgentJob(laAgent.getAgentJob());
+        ylLaAgentEntity.setAgentName(laAgent.getAgentName());
+        ylLaAgentEntity.setSapCode(laAgent.getSapCode());
+        ylLaAgentEntity.setManageCom(laAgent.getManageCom4());
+        ylLaAgentEntity.setAgentGroup(laAgent.getBranchAttr());
+        ylLaAgentEntity.setRepeatFlag("01");//调用wxy的返回接口
+        ylLaAgentEntity.setOperator(laAgent.getOperator());
+/*****************************************************************************/
+        ylLaAgentEntity.setAgentState("01");
+        ylLaAgentEntity.setBranchType("01");
+/*****************************************************************************/
+        Date date = new Date();
+        SimpleDateFormat df = new SimpleDateFormat("HH-mm-ss");
+        String time = df.format(date);
+        ylLaAgentEntity.setMakeDate(date);
+        ylLaAgentEntity.setMakeTime(time);
+        ylLaAgentEntity.setModifyDate(date);
+        ylLaAgentEntity.setModifyTime(time);
+
+        //存入数据库
+        int result = this.baseMapper.insert(ylLaAgentEntity);
+        if(result > 0){
+            return true;
+        }
+        return false;
+    }
+}

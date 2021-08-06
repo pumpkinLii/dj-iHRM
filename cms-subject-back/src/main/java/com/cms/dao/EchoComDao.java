796a14cb64e7f8615bfc397cb489762de8cc94c5
wangjiazhi <11111111@djbx.com>
2021-08-06 16:10:49 +0800

    更新团队

--- cms-subject-back/src/main/java/com/cms/dao/EchoComDao.java
+++
@@ -1,8 +0,0 @@
-package com.cms.dao;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.cms.entity.LdComNewEntity;
-import com.cms.entity.YlLaBranchGroupEntity;
-
-public interface EchoComDao extends BaseMapper<LdComNewEntity> {
-}

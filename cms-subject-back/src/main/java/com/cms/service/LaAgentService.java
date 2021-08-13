7556e64fba3b1413840b501d2b222e48076e800d
wangjiazhi <11111111@djbx.com>
2021-08-13 09:31:37 +0800

    资格证查询文件

--- cms-subject-back/src/main/java/com/cms/service/LaAgentService.java
+++
@@ -1,9 +0,0 @@
-package com.cms.service;
-
-import com.cms.pojo.LaAgentPojo;
-import com.cms.pojo.LaAgentUpdatePojo;
-
-public interface LaAgentService {
-    public boolean laAgentSubmit(LaAgentPojo laAgent);
-    public boolean laAgentUpdate(LaAgentUpdatePojo laAgent);
-}

486b2f69b86a84cb0561d374c4aeee65643d70e2
zhangchen <1329152979@qq.com>
2021-08-13 12:58:45 +0800

    personhold Url 修改

--- h5/src/api/personhold.js
+++ h5/src/api/personhold.js
@@ -1,57 +1,62 @@
 import request from '@/utils/request'
 
 // 查询人员信息
-export function queryPerson(data, page) {
+export function queryPerson(data,page){
   return request({
-    // url: 'http://10.11.114.33:9999/test/QueryStaff?limit=' + page.pageSize + '&' + 'page=' + page.currentPage,
     url: '/test/QueryStaff?limit=' + page.pageSize + '&' + 'page=' + page.currentPage,
-    method: 'post',
+    method:'post',
     data
   })
 }
+
+//修改人员信息  搁置
+// export function modifyPerson(data){
+//   return request({
+//     url:'',
+//     method:'post',
+//     data
+//   })
+// }
 // 模板下载
 
-export function download() {
+export function download(){
   return request({
-    // url: Vue.config.productionTip === true ? '/test/board' : 'http://10.11.114.128:9999/test/board',
-    url: '/test/board',
+    url: '/Board/YlInsertExcel',
     method: 'post',
-    responseType: 'blob'
+    responseType:'blob'
   })
 }
 
 // 导出
-export function download1(data) {
+export function download1(data){
   return request({
-    // url: Vue.config.productionTip === true ? '/test/ylLaAgentAttrExcelOut' : 'http://10.11.114.128:9999/test/ylLaAgentAttrExcelOut',
-    url: '/test/ylLaAgentAttrExcelOut',
-    method: 'post',
-    responseType: 'blob',
+    url:'/test/ylLaAgentAttrExcelOut',
+    method:'post',
+    responseType:'blob',
     data
   })
 }
 // 管理机构下拉列表
-export function xiala(abc) {
+export function xiala(abc){
   return request({
-    url: '/get/gets?Code=' + abc,
-    method: 'post'
+    url:'/get/gets?Code='+abc,
+    method:'post',
   })
 }
-export function xiala1() {
+export function xiala1(){
   return request({
-    url: '/get/gets?Code=86',
-    method: 'post'
+    url:'/get/gets?Code=86',
+    method:'post'
   })
 }
 // 团队下拉列表
-export function xiala3(data) {
+export function xiala3(data){
   return request({
-    // url: 'http://10.11.114.32:9999/test/QueryGroup',
-    url: '/test/QueryGroup',
-    // Vue.config.productionTip===true?'/test/QueryGroup':
-    method: 'post',
+    url:'/test/QueryGroup',
+    method:'post',
     data
   })
 }
-// Excel 导入  搁置
+//Excel 导入  搁置
+
 

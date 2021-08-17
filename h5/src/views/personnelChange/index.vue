a4b2eb8a85ef5fcc0597f83f912ca1cca4e4f14c
UR901006 <UR901006ZHANGJIAQI@DJBX.COM>
2021-08-17 16:45:22 +0800

    修改一些属性

--- h5/src/views/personnelChange/index.vue
+++ h5/src/views/personnelChange/index.vue
@@ -60,18 +60,18 @@
     </el-form>
     <el-divider />
     <div>
-      <el-table :data="table" stripe border fit height="300" :selection-change="handleSelectChange">
+      <el-table :data="table" stripe border fit height="300" @selection-change="handleSelectChange">
         <el-table-column type="selection" width="40" />
         <el-table-column label="人员工号" prop="agentCode" />
         <el-table-column label="人员姓名" prop="agentName" />
-        <el-table-column label="分公司" prop="gradeName" />
-        <el-table-column label="中心支公司" prop="manageCom2" />
-        <el-table-column label="团队代码" prop="manageCom3" />
-        <el-table-column label="团队名称" prop="manageCom4" />
+        <el-table-column label="分公司" prop="?" />
+        <el-table-column label="中心支公司" prop="?" />
+        <el-table-column label="团队代码" prop="branchAttr" />
+        <el-table-column label="团队名称" prop="branchName" />
         <el-table-column label="入司日期" prop="employDate" />
-        <el-table-column label="人员状态" prop="stateName" />
-        <el-table-column label="合同类型" prop="stateName" />
-        <el-table-column label="职级" prop="stateName" />
+        <el-table-column label="人员状态" prop="agentState" />
+        <el-table-column label="合同类型" prop="contractType" />
+        <el-table-column label="职级" prop="agentGrade" />
       </el-table>
       <!-- 分页 -->
       <div class="block" style="text-align: right;margin-top: 1rem">
@@ -158,21 +158,21 @@ export default {
     handleOpenChangeDialog() {
       this.$bus.$emit('OPEN_PERSON_CHANGE_DIALOG', this.selected)
     },
-    // 一页有几行
     handleSizeChange(size) {
       this.page.pageSize = size
-      this.$bus.$emit('refresh')
+      this.handleQuery()
     },
-    // 第几页
     handleCurrentChange(page) {
       this.page.currentPage = page
-      this.$bus.$emit('refresh')
+      this.handleQuery()
     },
     handleSelectChange(selection) {
-      console.log(selection)
+      this.selected = []
+      for (const item of selection) {
+        this.selected.push(item.agentCode)
+      }
     },
     getInitOptions() {
-      // 获取管理机构下拉菜单
       getManageComCode().then(
         r => {
           this.$log('成功获取管理机构下拉菜单请求', r)

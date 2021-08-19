15f738cd0c3f0dadb493a18b85cd1af2622b4509
wangyuan <2293520266@qq.com>
2021-08-19 11:01:49 +0800

    8/19 1.0

--- h5/src/components/PersonChange/PersonChangeDialog.vue
+++ h5/src/components/PersonChange/PersonChangeDialog.vue
@@ -1,37 +1,54 @@
 <template>
   <el-dialog title="人员异动" :visible.sync="config.dialogFormVisible">
-    <el-form :model="form">
-      <el-form-item label="目标中心支公司" :label-width="formLabelWidth">
-        <el-select v-model="form.manageCom" placeholder="目标中心支公司" style="width:60%;" autocomplete="off" @change="getAgentGroupList">
-          <el-option v-for="(option,index) in list.manageComList" :key="index" :label="option.name" :value="option.code">
-            <span style="float: left; color: #8492a6; font-size: 13px">{{ option.code }}</span>
-            <span style="float: right">{{ option.name }}</span>
-          </el-option>
-        </el-select>
-      </el-form-item>
-      <el-form-item label="目标团队" :label-width="formLabelWidth">
-        <el-select v-model="form.agentGroup" placeholder="目标团队" style="width:60%;" @change="getInformation">
-          <el-option v-for="(option,index) in list.agentGroupList" :key="index" :label="option.name" :value="option.branchAttr">
-            <span style="float: left; color: #8492a6; font-size: 13px">{{ option.branchAttr }}</span>
-            <span style="float: right">{{ option.name }}</span>
-          </el-option>
-        </el-select>
-      </el-form-item>
-      <el-form-item label="调动日期" :label-width="formLabelWidth">
-        <el-date-picker
-          v-model="form.modifyDate"
-          type="date"
-          placeholder="选择日期"
-          :disabled="true"
-          value-format="yyyy-MM-dd"
-        />
-      </el-form-item>
-      <el-form-item label="目标团队主管代码" :label-width="formLabelWidth">
-        <el-input v-model="form.adminCode" autocomplete="off" :disabled="true" />
-      </el-form-item>
-      <el-form-item label="目标团队主管姓名" :label-width="formLabelWidth">
-        <el-input v-model="form.adminName" autocomplete="off" :disabled="true" />
-      </el-form-item>
+    <el-form :model="form" label-width="130px">
+      <el-row>
+        <el-col :span="12">
+          <el-form-item label="目标中心支公司">
+            <el-select v-model="form.manageCom" placeholder="目标中心支公司" style="width:60%;" autocomplete="off" @change="getAgentGroupList">
+              <el-option v-for="(option,index) in list.manageComList" :key="index" :label="option.name" :value="option.code">
+                <span style="float: left; color: #8492a6; font-size: 13px">{{ option.code }}</span>
+                <span style="float: right">{{ option.name }}</span>
+              </el-option>
+            </el-select>
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="目标团队">
+            <el-select v-model="form.agentGroup" placeholder="目标团队" style="width:60%;" @change="getInformation">
+              <el-option v-for="(option,index) in list.agentGroupList" :key="index" :label="option.name" :value="option.branchAttr">
+                <span style="float: left; color: #8492a6; font-size: 13px">{{ option.branchAttr }}</span>
+                <span style="float: right">{{ option.name }}</span>
+              </el-option>
+            </el-select>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="12">
+          <el-form-item label="调动日期">
+            <el-date-picker
+              v-model="form.modifyDate"
+              style="width:60%;"
+              type="date"
+              placeholder="选择日期"
+              :disabled="true"
+              value-format="yyyy-MM-dd"
+            />
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="目标团队主管代码">
+            <el-input v-model="form.adminCode" autocomplete="off" :disabled="true" style="width:60%;" />
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="12">
+          <el-form-item label="目标团队主管姓名">
+            <el-input v-model="form.adminName" autocomplete="off" :disabled="true" style="width:60%;" />
+          </el-form-item>
+        </el-col>
+      </el-row>
     </el-form>
     <div slot="footer" class="dialog-footer">
       <el-button @click="config.dialogFormVisible = false">取 消</el-button>
@@ -61,8 +78,7 @@ export default {
       list: {
         manageComList: '',
         agentGroupList: ''
-      },
-      formLabelWidth: '180px'
+      }
     }
   },
   mounted() {

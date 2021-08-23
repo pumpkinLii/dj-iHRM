266d97c56e4aa459f2586900abba073b9330716d
UR901003 <DajiaAD@2233>
2021-08-23 10:51:06 +0800

    资格证模板下载

--- cms-subject-back/src/main/java/com/cms/controller/CertExcellBoardController.java
+++ cms-subject-back/src/main/java/com/cms/controller/CertExcellBoardController.java
@@ -4,12 +4,14 @@ import com.cms.pojo.IdCodePojo;
 import com.cms.service.NIdCodeService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.ss.util.CellRangeAddress;
 import org.apache.poi.ss.util.CellRangeAddressList;
 import org.apache.poi.xssf.usermodel.XSSFCellStyle;
 import org.apache.poi.xssf.usermodel.XSSFFont;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,12 +26,15 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URLEncoder;
 import java.util.List;
+import java.util.logging.Logger;
 
 @RestController
 @CrossOrigin
 @RequestMapping({"/cert"})
 @Api("养老人管模块课题")
+@Slf4j
 public class CertExcellBoardController {
+
 
     @Autowired
     private NIdCodeService nIdCodeService;
@@ -42,12 +47,15 @@ public class CertExcellBoardController {
 
     public void getData(HttpServletResponse response) throws FileNotFoundException {
         try {
+            log.info("项目启动");
             XSSFWorkbook workbook = new XSSFWorkbook();
             Sheet sheet = workbook.createSheet("资格证导入模板");
             sheet.createFreezePane(0,1,0,1);
             sheet.autoSizeColumn(0, true);            //默认列宽
             sheet.setDefaultColumnWidth(20);            //默认列宽
             sheet.setDefaultRowHeightInPoints(23);      //默认行高
+
+            log.debug("创建工作区样式，获取到sheet为：'{}' ",sheet);
 
             Row row0 = sheet.createRow(0);
             Cell cell0 = row0.createCell(0);
@@ -57,6 +65,8 @@ public class CertExcellBoardController {
             CellRangeAddress region=new CellRangeAddress(0, 0, 0, 7);//            firstRow 区域中第一个单元格的行号,lastRow 区域中最后一个单元格的行号,firstCol 区域中第一个单元格的列号,lastCol 区域中最后一个单元格的列号
             sheet.addMergedRegion(region);
 
+            log.debug("创建标题样式，获取到sheet为：'{}' ",sheet);
+            log.debug("创建标题样式，获取到cell为：'{}' ",cell0);
 
             String[] str = new String[]{"人员工号*","资格证书名称*","资格证书号*","发放日期*","补发日期","有效起期*","有效止期*","批准单位"};
             XSSFCellStyle textStyle = genTextStyle(workbook);//创建标题样式
@@ -67,6 +77,8 @@ public class CertExcellBoardController {
                 cell1.setCellStyle(textStyle);
             }
 
+            log.debug("创建表头样式，获取到sheet为：'{}'",sheet);
+
             for(int i=0;i<40;i++)
             {
                 if(i==1)
@@ -74,15 +86,15 @@ public class CertExcellBoardController {
                     CodeType(i,"certificatename",sheet);
                 }
             }
+
+            log.debug("创建数据样式，获取到sheet为：'{}'",sheet);
+
             ServletOutputStream outputStream=response.getOutputStream();
             response.setHeader("content-disposition","attachment;fileName="+ URLEncoder.encode("导入资格证模版.xlsx","UTF-8"));
             workbook.write(outputStream);
+            log.debug("获取输出流为：'{}'",outputStream);
             outputStream.flush();
             outputStream.close();
-//            FileOutputStream fos = new FileOutputStream(new File("C:\\Users\\LENOVO\\Desktop\\导入资格证模版.xlsx"));
-//            workbook.write(fos);
-//            fos.flush();
-//            fos.close();
         } catch (IOException var11) {
             var11.printStackTrace();
         }
@@ -136,13 +148,6 @@ public class CertExcellBoardController {
         DataValidationHelper helper = sheet.getDataValidationHelper();
         CellRangeAddressList addressList = new CellRangeAddressList(2, 500, i, i);
         String[] pos = list.toArray(new String[list.size()]);
-        if(i==27)
-        {
-            for(int k=0;k<pos.length;k++)
-            {
-                System.out.println(pos[k]);
-            }
-        }
         DataValidationConstraint constraint = helper.createExplicitListConstraint(pos);
         DataValidation dataValidation = helper.createValidation(constraint, addressList);
         sheet.addValidationData(dataValidation);

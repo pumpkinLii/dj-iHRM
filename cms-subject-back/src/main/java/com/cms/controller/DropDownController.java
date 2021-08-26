38b25c43005c3ab8895d69c8a5d35bce982abcf2
UR901008 <200911mm@>
2021-08-26 13:09:48 +0800

    接口整合

--- cms-subject-back/src/main/java/com/cms/controller/DropDownController.java
+++ cms-subject-back/src/main/java/com/cms/controller/DropDownController.java
@@ -1,14 +1,57 @@
 package com.cms.controller;
 
+import com.cms.service.EmpService;
+import com.cms.util.R;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.cms.service.EmpService;
+import com.cms.util.R;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin
 @RequestMapping("/login/get")
 @Api("下拉列表控制层")
 public class DropDownController {
+
+
+    /**
+     * 1 * @Author: zhangYiLong
+     * 2 * @Date: 2021/8/2 20:10
+     * 3 * @ClassName : EmpController
+     */
+    @RestController
+    @CrossOrigin
+    @RequestMapping("/postToRank")
+    @Api("岗位对应职级")
+    public class EmpController {
+        @Autowired
+        private EmpService empService;
+
+        @ApiOperation("岗位对应职级接口s")
+        @PostMapping("/manage")//1对应客户经理，0对应总监
+        public R SearchM() {
+            List list = empService.SearchM();
+            return R.ok().put("list",list);
+        }
+        @ApiOperation("岗位对应职级接口a")
+        @PostMapping("/director")
+        public R SearchG() {
+            List list = empService.SearchG();
+            return R.ok().put("list",list);
+        }
+    }
+
 
 }

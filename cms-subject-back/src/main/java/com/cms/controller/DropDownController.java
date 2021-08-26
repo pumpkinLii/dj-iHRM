1adfc2a0267116e71744fdb06c06a3ee43772a6c
wangjiazhi <11111111@djbx.com>
2021-08-26 13:17:33 +0800

    下拉控制层

--- cms-subject-back/src/main/java/com/cms/controller/DropDownController.java
+++ cms-subject-back/src/main/java/com/cms/controller/DropDownController.java
@@ -41,8 +41,8 @@ public class DropDownController {
      * 2 * @Date: 2021/8/2 20:10
      * 3 * @ClassName : EmpController
      */
-        @Autowired
-        private EmpService empService;
+    @Autowired
+    private EmpService empService;
     @Autowired
     IdCodeService idCodeService;
     @Autowired
@@ -53,74 +53,79 @@ public class DropDownController {
     ProxyComNewReturn proxyComNewReturn;
     @Autowired
     LaAgentGradeService laAgentGradeService;
+
     @ApiOperation("管理机构下拉列表")
     @PostMapping("/managecom")
     public R SearchMap() {
-        List list= magService.SearchMap();
-        List list1=(List)list.get(0);
-        List list2=(List) list.get(1);
-        List list3=(List)list.get(2);
-        List list4=(List)list.get(3);
-        List totallist=(List) list.get(4);
-        return R.ok().put("list1",list1).put("list2",list2).put("list3",list3).put("list4",list4).put("totallist",totallist);
+        List list = magService.SearchMap();
+        List list1 = (List) list.get(0);
+        List list2 = (List) list.get(1);
+        List list3 = (List) list.get(2);
+        List list4 = (List) list.get(3);
+        List totallist = (List) list.get(4);
+        return R.ok().put("list1", list1).put("list2", list2).put("list3", list3).put("list4", list4).put("totallist", totallist);
 
     }
+
     //下面这个是 有树状图的下拉列表 分级管理的模式 根据获取code的长度从而返回不同的
 //请求参数为“”的下拉款 我返回总的数据 请求参数为两位 就返回4位 同理进行操作
     @ApiOperation("树状图管理机构下拉列表")
     @PostMapping("/gets")
-    public R getMap(String Code){
+    public R getMap(String Code) {
         //这里进行条件的判断
-        if (StringUtils.isEmpty(Code)==true){
-            return R.ok().put("list",comnewSon.getHighManageCom());
-        }else {
-            return R.ok().put("list",comnewSon.getSonManageCom(Code));
+        if (StringUtils.isEmpty(Code) == true) {
+            return R.ok().put("list", comnewSon.getHighManageCom());
+        } else {
+            return R.ok().put("list", comnewSon.getSonManageCom(Code));
         }
     }
+
     @ApiOperation("children的结构管理机构")
     @PostMapping("/comresource")
-    public R getResource(){
-        return R.ok().put("result",proxyComNewReturn.Start());
+    public R getResource() {
+        return R.ok().put("result", proxyComNewReturn.Start());
     }
 
 
     @ApiOperation("码表资源接口")
     @PostMapping("/idcode")
-    public R getidResource(){
+    public R getidResource() {
 
-        return R.ok().put("resource",idCodeService.getResource());
+        return R.ok().put("resource", idCodeService.getResource());
     }
 
 
     @ApiOperation("职级下拉框接口")
     @PostMapping("/grade")
-    public R  getAgentGrade(){
-        return R.ok().put("list",laAgentGradeService.method());
+    public R getAgentGrade() {
+        return R.ok().put("list", laAgentGradeService.method());
     }
 
     @ApiOperation("下载新增接口")
     @PostMapping("/load")
     public R download(HttpServletResponse httpServletResponse, MultipartFile file) throws IOException {
-        OutputStream outputStream=new FileOutputStream("D:\\附件目录\\"+file.getOriginalFilename());
+        OutputStream outputStream = new FileOutputStream("D:\\附件目录\\" + file.getOriginalFilename());
         InputStream inputStream = file.getInputStream();
-        byte[] bytes=new byte[1024];
-        while ((inputStream.read(bytes))>0){
+        byte[] bytes = new byte[1024];
+        while ((inputStream.read(bytes)) > 0) {
             outputStream.write(bytes);
         }
         outputStream.flush();
         outputStream.close();
         return R.ok();
     }
-        @ApiOperation("岗位对应职级接口s")
-        @PostMapping("/manage")//1对应客户经理，0对应总监
-        public R SearchM() {
-            List list = empService.SearchM();
-            return R.ok().put("list",list);
-        }
-        @ApiOperation("岗位对应职级接口a")
-        @PostMapping("/director")
-        public R SearchG() {
-            List list = empService.SearchG();
-            return R.ok().put("list",list);
-        }
+
+    @ApiOperation("岗位对应职级接口s")
+    @PostMapping("/manage")//1对应客户经理，0对应总监
+    public R SearchM() {
+        List list = empService.SearchM();
+        return R.ok().put("list", list);
     }
+
+    @ApiOperation("岗位对应职级接口a")
+    @PostMapping("/director")
+    public R SearchG() {
+        List list = empService.SearchG();
+        return R.ok().put("list", list);
+    }
+}

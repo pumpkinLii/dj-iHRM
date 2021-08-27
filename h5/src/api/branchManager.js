04800341c617afcd1aebd3483b6338a25188818b
lijing <UR901012LIJING@DJBX.COM>
2021-08-27 15:50:03 +0800

    修改 5

--- h5/src/api/branchManager.js
+++ h5/src/api/branchManager.js
@@ -3,7 +3,7 @@ import request from '@/utils/request'
 // 查询团队信息 branchManager:负责人代码
 export function getManagerInfoByCode(data) {
   return request({
-    url: '/test/getGroupManager', // 待修改
+    url: '/login/back/getgroupmanager', // 待修改
     method: 'post',
     data
   })

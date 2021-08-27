c7fe2b60836cea668e34a36823af06b7f085db7e
zhangxiaocheng <ur901007zhangxiaocheng@djbx.com>
2021-08-27 16:41:09 +0800

    完善异动日期的正则表达式

--- cms-subject-back/src/main/java/com/cms/service/impl/YlLaAgentInfoChangeServiceImpl.java
+++ cms-subject-back/src/main/java/com/cms/service/impl/YlLaAgentInfoChangeServiceImpl.java
@@ -61,7 +61,7 @@ public class YlLaAgentInfoChangeServiceImpl extends ServiceImpl<YlLaAgentDao, Yl
         if((ylLaAgentChangePojo.getModifyDate() == null) || (ylLaAgentChangePojo.getModifyDate().equals(""))){
             return "您选择的异动日期为空！";
         }
-        if(!ylLaAgentChangePojo.getModifyDate().matches("\\d{4}-\\d{1,2}-\\d{2}")){
+        if(!ylLaAgentChangePojo.getModifyDate().matches("\\d{4}-\\d{1,2}-\\d{1,2}")){
             return "您输入的异动日期格式错误";
         }
         if((ylLaAgentChangePojo.getOperator() == null) || (ylLaAgentChangePojo.getOperator().equals(""))){
